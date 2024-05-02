79fdf3d4fa2a2eacde92812839fa093316c63164
ElyaNur <sitinurjanah_1601617043@mhs.unj.ac.id>
2024-05-02 09:33:02 +0700

    fix react/no-unescaped-entities

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -3,7 +3,7 @@ const About = () => {
         <div>
             <p className="mb-4">
                 In 2020, which was my final year of college, my final project involved building a website application,
-                and that's where I started to seriously learn programming and web development. Fast forward to the
+                and that&apos;s where I started to seriously learn programming and web development. Fast forward to the
                 present, I have had many opportunities to build software for various agencies such as <a
                 className="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
                 href="https://sbsbisa.com/" target="_blank" rel="noreferrer noopener"
@@ -34,12 +34,12 @@ const About = () => {
                    aria-label="Goverment agency (opens in a new tab)">RSUD Kota Tangerang</a>. I really enjoy building
                 applications, especially web-based ones, with various
                 latest technologies and of course that are stable and can be used in production environments. In my
-                spare time, I usually read news about the latest technology so that I don't fall behind the rapid
+                spare time, I usually read news about the latest technology so that I don&apos;t fall behind the rapid
                 development of technology.
             </p>
             <p>
-                When I'm not working, I usually hangout with my wife and two cats, read, clean the house,
-                and occasionally play the game Baldur's Gate 3.
+                When I&apos;m not working, I usually hangout with my wife and two cats, read, clean the house,
+                and occasionally play the game Baldur&apos;s Gate 3.
             </p>
         </div>
     );
--- src/components/header/header-description.tsx
+++ src/components/header/header-description.tsx
@@ -6,9 +6,10 @@ const HeaderDescription = () => {
             </h1>
             <h2 className="text-lg font-medium text-slate-900 dark:text-slate-200 sm:text-xl">Mid Level Fullstack
                 Developer</h2>
-            <p className="mt-4 leading-normal">I'm a mid level fullstack developer with 3 years of experience. I'm
+            <p className="mt-4 leading-normal">I&apos;m a mid level fullstack developer with 3 years of experience.
+                I&apos;m
                 passionate about building scalable and maintainable web applications. I have experience in building web
-                applications using React, Laravel, and Node.js. I'm also familiar with AWS and Docker.</p>
+                applications using React, Laravel, and Node.js. I&apos;m also familiar with AWS and Docker.</p>
         </>
     );
 };
