1c2a9efc20acb6588f05105c86af61f0927ff4ac
ElyaNur <sitinurjanah_1601617043@mhs.unj.ac.id>
2024-06-06 18:44:44 +0700

    change line on contact component

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -5,8 +5,9 @@ const Contact = () => {
     return (
         <div>
             <h3 className="text-2xl font-bold leading-tight">Get In Touch</h3>
-            <p>Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a
-                question or just want to say hi, I’ll try my best to get back to you!</p>
+            <p>I&apos;m actively seeking opportunities to gain new skills and experience. Feel free to reach out if you
+                have
+                any projects or tasks that I could learn from!</p>
             <Button className="mt-5 text-slate-900 font-bold" asChild>
                 <a href="mailto:charis.aceh@gmail.com" rel="noopener noreferrer" target="_blank">
                     Say hello!
